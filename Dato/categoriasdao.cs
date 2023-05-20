c7de4c88d11de06bff9df01d1317023c7a64761c
RafaelMartinez-3 <s21120238@alumnos.itsur.edu.mx>
2023-05-20 13:44:14 -0600

    Subiendo EL catalogo de la tabla categorias

---
+++ Dato/categoriasdao.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using MySql.Data.MySqlClient;
+
+namespace Datos
+{
+    public class categoriasdao
+    {
+        public List<Categorias> GetAll()
+        {
+            List<Categorias> lista = new List<Categorias>();
+            if (Conexion.Conectar())
+            {
+                try
+                {
+                    String select = "select * from categories;";
+                    DataTable dt = new DataTable();
+                    MySqlCommand sentencia = new MySqlCommand();
+                    sentencia.CommandText = select;
+                    sentencia.Connection = Conexion.conexion;
+                    MySqlDataAdapter da = new MySqlDataAdapter();
+                    da.SelectCommand = sentencia;
+                    da.Fill(dt);
+                    // Objeto Category auxiliar para asignar null como 
+                    lista.Add(new Categorias());
+                    foreach (DataRow fila in dt.Rows)
+                    {
+                        Categorias categoria = new Categorias(Convert.ToInt32(fila["categoryid"]),
+                            fila["categoryname"].ToString(), fila["description"].ToString());
+                        lista.Add(categoria);
+                    }
+                    return lista;
+                }
+                finally
+                {
+                    Conexion.Desconectar();
+                }
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public int Insert(Categorias categoria, bool update)
+        {
+            if (Conexion.Conectar())
+            {
+                try
+                {
+                    MySqlCommand sentencia = new MySqlCommand();
+                    if (update)
+                    {
+                        String select = @"Update categories set CategoryName = @name,
+                        Description = @description where CategoryId = @id;";
+
+                        sentencia.CommandText = select;
+                        sentencia.Parameters.AddWithValue("@name", categoria.CategoryName);
+                        sentencia.Parameters.AddWithValue("@description", categoria.CategoryDescription);
+                        sentencia.Parameters.AddWithValue("@id", categoria.CategoryID);
+                    }
+                    else
+                    {
+                        String select = @"insert into categories (categoryname,description) 
+                            values (@name,@description);
+                            select last_insert_id();";
+
+                        sentencia.CommandText = select;
+                        sentencia.Parameters.AddWithValue("@description", categoria.CategoryDescription);
+                        sentencia.Parameters.AddWithValue("@name", categoria.CategoryName);
+
+                    }
+
+                    sentencia.Connection = Conexion.conexion;
+                    object ob = sentencia.ExecuteScalar();
+
+                    return Convert.ToInt32(ob);
+                }
+                finally
+                {
+                    Conexion.Desconectar();
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
